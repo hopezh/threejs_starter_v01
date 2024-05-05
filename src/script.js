6a7114a1e79167b99d393843d0de118afd009c1f
hopezh <hope.zh@gmail.com>
2024-05-05 21:04:35 +0800

    add demo files

--- src/script.js
+++ src/script.js
@@ -1,225 +1,249 @@
-import './style.css'
+// Import libraries
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import { RectAreaLightHelper } from 'three/addons/helpers/RectAreaLightHelper.js'
-import { RectAreaLightUniformsLib } from 'three/addons/lights/RectAreaLightUniformsLib.js'
-import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
-import { Sky } from 'three/addons/objects/Sky.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { Rhino3dmLoader } from 'three/examples/jsm/loaders/3DMLoader'
+import rhino3dm from 'rhino3dm'
+import { RhinoCompute } from 'rhinocompute'
 
-/**
- * Base
- */
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
+// reference the definition
+const definitionName = 'SampleGHClash.gh'
 
-// Scene
-const scene = new THREE.Scene()
+// globals
+let rhino, definition, doc
+rhino3dm().then(async m => {
+    console.log('Loaded rhino3dm.')
+    rhino = m // global
 
-/**
- * Sizes
- */
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
+    RhinoCompute.url = getAuth('RHINO_COMPUTE_URL') // RhinoCompute server url. Use http://localhost:8081/ if debugging locally.
+    RhinoCompute.apiKey = getAuth('RHINO_COMPUTE_KEY')  // RhinoCompute server api key. Leave blank if debugging locally.
 
-window.addEventListener('resize', () => {
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+    // source a .gh / .ghx file in the same directory
+    let url = definitionName
+    let res = await fetch(url)
+    let buffer = await res.arrayBuffer()
+    definition = new Uint8Array(buffer)
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
-
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    init()
+    compute()
 })
 
-// helpers
-const gridHelper = new THREE.GridHelper(10, 10, 0x0000ff, 0x808080)
-scene.add(gridHelper)
+async function compute() {
 
-const axisHeler = new THREE.AxesHelper(1)
-scene.add(axisHeler)
+    // create meshes
+    const mainSphere = new THREE.SphereGeometry(10, 32, 32)
+    const material = new THREE.MeshBasicMaterial({ wireframe: true, color: 0x000000 })
+    const mainMesh = new THREE.Mesh(mainSphere, material)
+    mainMesh.rotateOnWorldAxis(new THREE.Vector3(1, 0, 0), THREE.MathUtils.degToRad(-90));
+    scene.add(mainMesh)
 
-// ambient light
-scene.add(new THREE.AmbientLight(0x222222))
+    // create 3dm sphere
+    const rhinoMesh = rhino.Mesh.createFromThreejsJSON({ data: mainSphere })
+    const rhinoMeshData = JSON.stringify(rhinoMesh.encode())
 
-// point light
-// const pointLight = new THREE.PointLight(0xffffff, 10, 0, 0);
-// pointLight.position.set(1, 2, -3)
-// scene.add(pointLight);
+    // create collision spheres
+    const rhinoCollisionMeshData = []
+    const pointsGeometry = new THREE.BufferGeometry()
+    const positionBuffer = new Float32Array(3 * 200)
 
+    for (let i = 0; i < 200; i++) {
+        const x = Math.random() * (20 - -20) + -20
+        const y = Math.random() * (20 - -20) + -20
+        const z = Math.random() * (20 - -20) + -20
 
-// rect area light
-RectAreaLightUniformsLib.init()
+        //create 3js clash sphere
+        const clashSphere = new THREE.SphereGeometry(2, 10, 10)
+        clashSphere.translate(x, y, z)
 
-const rectLight_1 = new THREE.RectAreaLight(0xff0000, 5, 4, 10)
-rectLight_1.position.set(-5, 4, -5)
-rectLight_1.lookAt(0, 4, -5)
-scene.add(rectLight_1)
+        //create 3dm clash sphere
+        const clashMesh = rhino.Mesh.createFromThreejsJSON({ data: clashSphere })
+        const clashMeshData = JSON.stringify(clashMesh.encode())
+        rhinoCollisionMeshData.push(clashMeshData)
 
-const rectLight_2 = new THREE.RectAreaLight(0x00ff00, 5, 4, 10)
-rectLight_2.position.set(-5, 4, 0)
-rectLight_2.lookAt(0, 4, 0)
-scene.add(rectLight_2)
-
-const rectLight_3 = new THREE.RectAreaLight(0x0000ff, 5, 4, 10)
-rectLight_3.position.set(-5, 4, 5)
-rectLight_3.lookAt(0, 4, 5)
-scene.add(rectLight_3)
-
-scene.add(new RectAreaLightHelper(rectLight_1));
-scene.add(new RectAreaLightHelper(rectLight_2));
-scene.add(new RectAreaLightHelper(rectLight_3));
-
-
-// floor
-const geoFloor = new THREE.BoxGeometry(100, 0.1, 100);
-const matStdFloor = new THREE.MeshStandardMaterial({
-    color: 0xbcbcbc,
-    roughness: 0.1,
-    metalness: 0,
-});
-const mshStdFloor = new THREE.Mesh(geoFloor, matStdFloor);
-mshStdFloor.position.y = -1;
-scene.add(mshStdFloor);
-
-
-/**
- * Camera
- */
-// Base camera
-const camera = new THREE.PerspectiveCamera(
-    75,
-    sizes.width / sizes.height,
-    0.1,
-    1000
-)
-camera.position.set(6, 0, 4)
-scene.add(camera)
-
-// Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
-
-/**
- * Cube
- */
-const cubeGeo = new THREE.BoxGeometry(1, 1, 1)
-// const cubeMat = new THREE.MeshLambertMaterial({
-//     color: 0xffa500,
-//     opacity: 0.5,
-//     side: THREE.DoubleSide,
-//     transparent: true
-// })
-const cubeMat = new THREE.MeshStandardMaterial({
-    color: 0xffffff,
-    roughness: 0,
-    metalness: 0,
-})
-const cube = new THREE.Mesh(cubeGeo, cubeMat)
-// cube.position.y = 1
-scene.add(cube)
-
-/**
- * Renderer
- */
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-    antialias: true,
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
-function initSky() {
-
-    // Add Sky
-    const sky = new Sky();
-    sky.scale.setScalar(450000);
-    scene.add(sky);
-
-    const sun = new THREE.Vector3();
-
-    /// GUI
-
-    const effectController = {
-        turbidity: 1,
-        rayleigh: 3,
-        mieCoefficient: 0.005,
-        mieDirectionalG: 0.7,
-        elevation: 1,
-        azimuth: -150,
-        exposure: renderer.toneMappingExposure
-    };
-
-    function guiChanged() {
-
-        const uniforms = sky.material.uniforms;
-        uniforms['turbidity'].value = effectController.turbidity;
-        uniforms['rayleigh'].value = effectController.rayleigh;
-        uniforms['mieCoefficient'].value = effectController.mieCoefficient;
-        uniforms['mieDirectionalG'].value = effectController.mieDirectionalG;
-
-        const phi = THREE.MathUtils.degToRad(90 - effectController.elevation);
-        const theta = THREE.MathUtils.degToRad(effectController.azimuth);
-
-        sun.setFromSphericalCoords(1, phi, theta);
-
-        uniforms['sunPosition'].value.copy(sun);
-
-        renderer.toneMappingExposure = effectController.exposure;
-        renderer.render(scene, camera);
+        positionBuffer[i * 3] = x
+        positionBuffer[i * 3 + 1] = y
+        positionBuffer[i * 3 + 2] = z
 
     }
 
-    const gui = new GUI();
+    //vizualize the position of the clash spheres
+    pointsGeometry.setAttribute('position', new THREE.BufferAttribute(positionBuffer, 3))
+    const points = new THREE.Points(pointsGeometry, new THREE.PointsMaterial({ color: 0xff0000, sizeAttenuation: false, size: 3 }))
+    points.rotateOnWorldAxis(new THREE.Vector3(1, 0, 0), THREE.MathUtils.degToRad(-90))
+    scene.add(points)
 
-    gui.add(effectController, 'turbidity', 0.0, 20.0, 0.1).onChange(guiChanged);
-    gui.add(effectController, 'rayleigh', 0.0, 4, 0.001).onChange(guiChanged);
-    gui.add(effectController, 'mieCoefficient', 0.0, 0.1, 0.001).onChange(guiChanged);
-    gui.add(effectController, 'mieDirectionalG', 0.0, 1, 0.001).onChange(guiChanged);
-    gui.add(effectController, 'elevation', 0, 90, 0.1).onChange(guiChanged);
-    gui.add(effectController, 'azimuth', - 180, 180, 0.1).onChange(guiChanged);
-    gui.add(effectController, 'exposure', 0, 1, 0.0001).onChange(guiChanged);
+    // format data
+    let param1 = new RhinoCompute.Grasshopper.DataTree('mainMesh')
+    param1.append([0], [rhinoMeshData])
+    let param2 = new RhinoCompute.Grasshopper.DataTree('collisionMeshes')
+    param2.append([0], rhinoCollisionMeshData)
 
-    guiChanged();
+    // Add all params to an array
+    let trees = []
+    trees.push(param1)
+    trees.push(param2)
+
+    // Call RhinoCompute
+
+    const res = await RhinoCompute.Grasshopper.evaluateDefinition(definition, trees)
+
+    console.log(res)
+
+    collectResults(res)
 
 }
-
-initSky()
-
 
 /**
- * Animate
+ * Parse response
  */
-const clock = new THREE.Clock()
-let lastElapsedTime = 0
+function collectResults(responseJson) {
 
-const tick = () => {
-    const elapsedTime = clock.getElapsedTime()
-    const deltaTime = elapsedTime - lastElapsedTime
-    lastElapsedTime = elapsedTime
+    const values = responseJson.values
 
-    // rotate cube
-    cube.rotation.x += 0.005
-    cube.rotation.y += 0.005
-    cube.rotation.z += 0.005
+    // clear doc
+    if (doc !== undefined)
+        doc.delete()
 
-    // rotate rectLight_2
-    // rectLight_2.position.x += Math.sin(Math.PI * 0.01)
-    // rectLight_2.position.z += Math.sin(Math.PI * 0.01)
+    //console.log(values)
+    doc = new rhino.File3dm()
 
-    // Update controls
-    controls.update()
+    // for each output (RH_OUT:*)...
+    for (let i = 0; i < values.length; i++) {
+        // ...iterate through data tree structure...
+        for (const path in values[i].InnerTree) {
+            const branch = values[i].InnerTree[path]
+            // ...and for each branch...
+            for (let j = 0; j < branch.length; j++) {
+                // ...load rhino geometry into doc
+                const rhinoObject = decodeItem(branch[j])
+                if (rhinoObject !== null) {
+                    doc.objects().add(rhinoObject, null)
+                }
+            }
+        }
+    }
 
-    // Render
-    renderer.render(scene, camera)
+    if (doc.objects().count < 1) {
+        console.error('No rhino objects to load!')
+        showSpinner(false)
+        return
+    }
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+    // set up loader for converting the results to threejs
+    const loader = new Rhino3dmLoader()
+    loader.setLibraryPath('https://unpkg.com/rhino3dm@7.15.0/')
+
+    const resMaterial = new THREE.MeshBasicMaterial({ wireframe: true, color: 0x00ff00 })
+
+    // load rhino doc into three.js scene
+    const buffer = new Uint8Array(doc.toByteArray()).buffer
+    loader.parse(buffer, function(object) {
+
+        // add material to resulting meshes
+        object.traverse(child => {
+            child.material = resMaterial
+            child.rotateOnWorldAxis(new THREE.Vector3(1, 0, 0), THREE.MathUtils.degToRad(-90))
+        })
+
+        // add object graph from rhino model to three.js scene
+        scene.add(object)
+
+        // hide spinner
+        showSpinner(false)
+
+    }, (error) => {
+        console.error(error)
+    })
 }
 
-tick()
+/**
+ * Attempt to decode data tree item to rhino geometry
+ */
+function decodeItem(item) {
+    const data = JSON.parse(item.data)
+    if (item.type === 'System.String') {
+        // hack for draco meshes
+        try {
+            return rhino.DracoCompression.decompressBase64String(data)
+        } catch { } // ignore errors (maybe the string was just a string...)
+    } else if (typeof data === 'object') {
+        return rhino.CommonObject.decode(data)
+    }
+    return null
+}
+
+function getAuth(key) {
+    let value = localStorage[key]
+    if (value === undefined) {
+        const prompt = key.includes('URL') ? 'Server URL' : 'Server API Key'
+        value = window.prompt('RhinoCompute ' + prompt)
+        if (value !== null) {
+            localStorage.setItem(key, value)
+        }
+    }
+    return value
+}
+
+/**
+ * Shows or hides the loading spinner
+ */
+function showSpinner(enable) {
+    if (enable)
+        document.getElementById('loader').style.display = 'block'
+    else
+        document.getElementById('loader').style.display = 'none'
+}
+
+// BOILERPLATE //
+// declare variables to store scene, camera, and renderer
+let scene, camera, renderer
+
+function init() {
+
+    // Rhino models are z-up, so set this as the default
+    THREE.Object3D.DefaultUp = new THREE.Vector3(0, 0, 1)
+
+    // create a scene and a camera
+    scene = new THREE.Scene()
+    scene.background = new THREE.Color(1, 1, 1)
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+    camera.position.y = 30
+    camera.position.z = 30
+
+    // create the renderer and add it to the html
+    renderer = new THREE.WebGLRenderer({ antialias: true })
+    renderer.setPixelRatio(window.devicePixelRatio)
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    document.body.appendChild(renderer.domElement)
+
+    // add some controls to orbit the camera
+    const controls = new OrbitControls(camera, renderer.domElement)
+
+    // add a directional light
+    const directionalLight = new THREE.DirectionalLight(0xffffff)
+    directionalLight.intensity = 2
+    scene.add(directionalLight)
+
+    const ambientLight = new THREE.AmbientLight()
+    scene.add(ambientLight)
+
+    // handle changes in the window size
+    window.addEventListener('resize', onWindowResize, false)
+
+    animate()
+
+}
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    animate()
+}
+
+// function to continuously render the scene
+function animate() {
+
+    requestAnimationFrame(animate)
+    renderer.render(scene, camera)
+
+}
